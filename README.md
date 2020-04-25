59c8d9e5fe172f246d1e55b4c71e81eb0a8933e8
basicgts <basicgts@gmail.com>
2020-04-25 15:47:57 +0700

    first commit

--- README.md
+++ README.md
@@ -1,24 +1,40 @@
-# gear-x-liff-menu
+# LFFE MENU and Firebase cloud functions use in dialog-flow ,for hackathon project 
+### Features
+Create for hackathon project 
+# How to install
+Use powershell or cmd and type by order, please see below.
+- `git clone https://github.com/kantinanm/gear-x-liff-menu.git`
+- `cd gear-x-liff-menu`
+- > install package dependency in this project.
+    `npm install`
+- > install package dependency in cloud functions  project.
+    `cd functions`
+    `npm install`
+- > create config.js and modify value (in cloud functions folder).
+  `cp config.js.default config.js` 
+  > In windows use command `copy config.js.default config.js` 
+  > at config.js file to modify value, 
+  ```javascript
+     exports.user = ''; //reg user
+     exports.password = ''; // reg password
 
-## Project setup
-```
-npm install
-```
+- Go to firebase console , choose your project and create Cloud Firestore , set region east-2
 
-### Compiles and hot-reloads for development
-```
-npm run serve
-```
+# Deployment
+- `cd gear-x-liff-menu`
+- follow command below.
+  >  `firebase login `
+- Edit value in firebaserc.
+    ```javascript
+        {
+            "projects": {
+                "default": "project id " // your project id
+            }
+        };
+- follow command below.
+  >  `firebase projects:list `for check current folder project is have been mapping from your project id. 
+- build vue for prepair to dyploy hosting use command below.
+  >  `npm run build ` to create dist folder.
+- Let go deploy command below.
+ >  `firebase deploy --only functions ` 
 
-### Compiles and minifies for production
-```
-npm run build
-```
-
-### Lints and fixes files
-```
-npm run lint
-```
-
-### Customize configuration
-See [Configuration Reference](https://cli.vuejs.org/config/).
--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,83 @@
-const functions = require('firebase-functions');
+'use strict';
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
+const functions = require('firebase-functions');
+const {
+  WebhookClient,
+  Payload
+} = require('dialogflow-fulfillment');
+const admin = require('firebase-admin');
+admin.initializeApp({
+  credential: admin.credential.applicationDefault()
+});
+
+// const serviceAccount = require('./key.json')
+// admin.initializeApp({
+//   credential: admin.credential.cert(serviceAccount),
+//   databaseURL: 'https://pizzanulok-vfwchf.firebaseio.com'
 // });
+
+
+
+process.env.DEBUG = 'dialogflow:debug'; // enables lib debugging statements
+
+
+exports.dialogflowFirebaseFulfillment = functions.https.onRequest((request, response) => {
+  const agent = new WebhookClient({
+    request,
+    response
+  });
+  console.log('Dialogflow Request headers: ' + JSON.stringify(request.headers));
+  console.log('Dialogflow Request body: ' + JSON.stringify(request.body));
+
+  function welcome(agent) {
+    agent.add(`Welcome to my agent!`);
+  }
+
+  function fallback(agent) {
+    agent.add(`I didn't understand`);
+    agent.add(`I'm sorry, can you try again?`);
+  }
+
+
+
+  function register(agent) {
+    const params = agent.parameters
+    const studentID = params.stdID 
+    const studentCard = params.stdCard 
+    agent.add(`รหัสนิสิตของคุณคือ! ${studentID} และหมายเลขบัตรประจำตัวประชาชนของคุณคือ ${studentCard} ใช่หรือไม่ `)
+
+    const payloadJson = {
+        "type": "template",
+        "altText": "this is a confirm template",
+        "template": {
+          "type": "confirm",
+          "actions": [
+            {
+              "type": "message",
+              "label": "Yes",
+              "text": "สักครู่นะค่ะ"
+            },
+            {
+              "type": "message",
+              "label": "No",
+              "text": "แล้วค่อยกลับมาลงทะเบียนกับเราใหม่นะค่ะ"
+            }
+          ],
+          "text": "ยืนยันการลงทะเบียนนะค่ะ"
+        }
+      }
+    let payload = new Payload(`LINE`, payloadJson, { sendAsMessage: true });
+
+    agent.add(payload);
+
+  }
+
+  
+
+  // Run the proper function handler based on the matched Dialogflow intent name
+  let intentMap = new Map();
+  intentMap.set('Default Welcome Intent', welcome);
+  intentMap.set('Default Fallback Intent', fallback);
+  intentMap.set('studentinfo', register); //ชื่อ intent ที่มัทสร้างและกำหนด ใน dialogflow
+  return agent.handleRequest(intentMap);
+});
