dcde488763d2c5d998ece0e67f78cbbe25f6b35b
basicgts <basicgts@gmail.com>
2020-04-26 14:53:02 +0700

    update firebase-config.js.template

--- src/firebase-config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  apiKey: "AIzaSyAJo-a4brjmJC3O7_Oa9N9zINLOidYFSfw",
-  authDomain: "nu-gear-x-assistant.firebaseapp.com",
-  databaseURL: "https://nu-gear-x-assistant.firebaseio.com",
-  projectId: "nu-gear-x-assistant",
-  storageBucket: "nu-gear-x-assistant.appspot.com",
-  messagingSenderId: "943492024799",
-  appId: "1:943492024799:web:6f3ba42eb068ddc5a81c6a"
-  };
